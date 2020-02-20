260bfb8b3098a9f7066b2cf61a37b74ccf4ebc18
Baknik <rks3159@gmail.com>
2020-02-20 16:36:16 -0700

    Added laser satellite

---
+++ Assets/_Content/Components/Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [Header("Settings")]
+    public LineRenderer LineRenderer;
+    public float Range;
+    public float TickFrequency;
+    public float AimSpeed;
+    public Damage BaseTickDamage;
+    public float AddedDamagePerTick;
+
+    [HideInInspector]
+    public double LastTickTime;
+    [HideInInspector]
+    public int NumConsecutiveTicks;
+
+    void Start()
+    {
+        this.LastTickTime = 0d;
+        this.NumConsecutiveTicks = 0;
+    }
+}
---
+++ Assets/_Content/Systems/LaserSystem.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+
+public class LaserSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Laser laser) =>
+        {
+            Enemy closestEnemy = null;
+            float closestEnemyDistance = 0f;
+            Entities.ForEach((Enemy enemy) =>
+            {
+                float distanceToEnemy = Vector3.Distance(laser.transform.position, enemy.transform.position);
+                if (closestEnemy == null)
+                {
+                    closestEnemy = enemy;
+                    closestEnemyDistance = distanceToEnemy;
+                }
+                else if (distanceToEnemy < closestEnemyDistance)
+                {
+                    closestEnemy = enemy;
+                    closestEnemyDistance = distanceToEnemy;
+                }
+            });
+            
+            // Rotate towards enemy
+            if (closestEnemy != null && closestEnemyDistance <= laser.Range)
+            {
+                var q = Quaternion.LookRotation(closestEnemy.transform.position - laser.transform.position);
+                laser.transform.rotation = Quaternion.RotateTowards(laser.transform.rotation, q, laser.AimSpeed * Time.DeltaTime);
+            }
+
+            // Laser
+            Vector3 currentEndPosition = laser.LineRenderer.GetPosition(1);
+            if (closestEnemy != null && closestEnemyDistance <= laser.Range)
+            {
+                float sightAngle = Vector3.Angle(closestEnemy.transform.position - laser.transform.position, laser.transform.forward);
+                if (sightAngle <= 5f)
+                {
+                    // Laser length
+                    laser.LineRenderer.SetPosition(1, new Vector3(currentEndPosition.x, (5f * closestEnemyDistance) - 0.5f, currentEndPosition.z));
+
+                    // Tick
+                    if (((Time.ElapsedTime - laser.LastTickTime) >= laser.TickFrequency))
+                    {
+                        Health enemyHealth = null;
+                        Entities.ForEach((Enemy enemy, Health health) =>
+                        {
+                            if (enemy == closestEnemy)
+                            {
+                                enemyHealth = health;
+                            }
+                        });
+                        if (enemyHealth != null)
+                        {
+                            Damage tickDamage = new Damage()
+                            {
+                                Amount = laser.BaseTickDamage.Amount + (laser.AddedDamagePerTick * (float)laser.NumConsecutiveTicks),
+                                Type = laser.BaseTickDamage.Type
+                            };
+                            enemyHealth.Damage.Enqueue(tickDamage);
+                            laser.NumConsecutiveTicks++;
+                            laser.LastTickTime = Time.ElapsedTime;
+                        }
+                    }
+                }
+                else
+                {
+                    laser.LineRenderer.SetPosition(1, new Vector3(currentEndPosition.x, 0f, currentEndPosition.z));
+                    laser.NumConsecutiveTicks = 0;
+                }
+            }
+            else
+            {
+                laser.LineRenderer.SetPosition(1, new Vector3(currentEndPosition.x, 0f, currentEndPosition.z));
+                laser.NumConsecutiveTicks = 0;
+            }
+        });
+    }
+}
